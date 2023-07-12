0fc1a6d05eacf2d2703a40a3568866a9eb2028f9
Vitor Zerbeto <vitor.zerbeto@ifood.com.br>
2023-07-12 09:36:15 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>React server components example</h1>
+    </main>
+  );
+}
